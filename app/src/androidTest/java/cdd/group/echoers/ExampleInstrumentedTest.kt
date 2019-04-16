2178a26147624d1ab51fc3cedcbf31338f4bc06d
Raphael Zhang <fangtao.zhang@cdd.group>
2019-04-16 21:58:53 +0800

    1、整合三方包

--- app/src/androidTest/java/cdd/group/echoers/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/cdd/group/echoers/ExampleInstrumentedTest.kt
@@ -1,8 +1,7 @@
 package cdd.group.echoers
 
-import androidx.test.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,7 +17,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().context
         assertEquals("cdd.group.echoers", appContext.packageName)
     }
 }
