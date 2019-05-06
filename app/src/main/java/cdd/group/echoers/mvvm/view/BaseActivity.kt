3d2e0dae955df375322c8aa825d6257fcceda52b
Raphael Zhang <fangtao.zhang@cdd.group>
2019-05-06 21:43:57 +0800

    1、新增loading对话框 2、同步最新版本的library

--- app/src/main/java/cdd/group/echoers/mvvm/view/BaseActivity.kt
+++ app/src/main/java/cdd/group/echoers/mvvm/view/BaseActivity.kt
@@ -2,6 +2,7 @@ package cdd.group.echoers.mvvm.view
 
 import cdd.group.echoers.api.Api
 import cdd.group.echoers.api.END_POINT
+import com.echoers.library.components.loading.LoadingDialog
 import com.echoers.library.http.ApiFactory
 import com.echoers.library.mvvm.view.AbsCompactActivity
 
@@ -17,7 +18,31 @@ import com.echoers.library.mvvm.view.AbsCompactActivity
  **/
 abstract class BaseActivity: AbsCompactActivity() {
 
+    private var loadingDialog = LoadingDialog()
+
     val repository by lazy {
         ApiFactory.instance(this).createApi(Api::class.java, END_POINT)
+    }
+
+    /**
+     * 显示加载对话框
+     */
+    fun showLoadingDialog() {
+        loadingDialog.dialog?.let {
+            if (!it.isShowing) {
+                loadingDialog.show(supportFragmentManager, null)
+            }
+        }
+    }
+
+    /**
+     * 取消loading显示
+     */
+    fun dismissLoadingDialog() {
+        loadingDialog.dialog?.let {
+            if (it.isShowing) {
+                loadingDialog.dismiss()
+            }
+        }
     }
 }
